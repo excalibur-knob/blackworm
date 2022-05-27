d54d7c706b625e6c0bd4423700c17df2878b2fae
xdiz <44671333+excalibur-knob@users.noreply.github.com>
2022-05-28 00:49:05 +0400

    initial commit

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ let urlBtn = document.querySelector("#urlBtn")
 let srchBtn = document.querySelector("#srchBtn")
 let inp1 = document.querySelector("#input1")
 let inp2 = document.querySelector("#input2")
-
+let srchIcon = document.querySelector(".srchIcon")
+let scanIcon = document.querySelector(".scanIcon")
 urlBtn.onclick = () => {
-inp1.style.display = "block";
-inp2.style.display = "none";
+    srchIcon.style.display = "block";
+    scanIcon.style.display = "none"
+    urlBtn.classList.toggle("active")
+    srchBtn.classList.toggle("active")
+    inp1.style.display = "block";
+    inp2.style.display = "none";
 }
 srchBtn.onclick = () => {
+    srchIcon.style.display = "none";
+    scanIcon.style.display = "block"
+    srchBtn.classList.toggle("active")
+    urlBtn.classList.toggle("active")
     inp1.style.display = "none";
     inp2.style.display = "block";
 }
+
