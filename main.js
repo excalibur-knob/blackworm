2adc83cabcaca362c42fcf60911f115f9cbc9f45
xdiz <44671333+excalibur-knob@users.noreply.github.com>
2022-05-31 14:22:20 +0400

    initial commit

--- main.js
+++ main.js
@@ -21,3 +21,22 @@ srchBtn.onclick = () => {
     inp2.style.display = "block";
 }
 
+//navbar code
+const navSlide = () => {
+    const drop = document.querySelector(".drop")
+    const nav = document.querySelector(".nav-links")
+    const navLinks = document.querySelectorAll('.nav-links li')
+    drop.addEventListener('click' , ()=>{
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+            link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.3}s`
+            }
+        })
+    })
+    
+}
+navSlide();
+ 
