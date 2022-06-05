407e58d0e71927a8ea3ea769d4615fc0a83505f8
xdiz <44671333+excalibur-knob@users.noreply.github.com>
2022-06-05 14:44:22 +0400

    initial commit

--- worker1.js
+++ worker1.js
@@ -3,9 +3,9 @@ let failed = false;
 onmessage = function(e) {
  
      let database = ["blackworm.com"]
-    //  for(let i = 0;i<100000;i++){
-    //      console.log(i)
-    //  }
+     for(let i = 0;i<100000;i++){
+         console.log(i)
+     }
     console.log(e.data)
   if(e.data == database[0]){
     postMessage(failed)
