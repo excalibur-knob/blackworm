32cc75175d8c283bca0e1fe4ed9b8e29f8e4859e
xdiz <44671333+excalibur-knob@users.noreply.github.com>
2022-06-20 21:25:53 +0400

    initial commit

--- worker1.js
+++ worker1.js
@@ -1,35 +1,33 @@
-
-
-onmessage = function(e) {
-     let finished =false
-     let notFound = true
-     let data = e.data.toLowerCase()
-     let validSites= ["youtube.com","facebook.com"]
-     let notValidSites=["virus.com","stepn-eventos.com"]
-    //  for(let i = 0;i<100000;i++){
-    //      console.log(i)
-    //  }
+onmessage = function (e) {
+    let finished = false
+    let notFound = true
+    let data = e
+        .data
+        .toLowerCase()
+    let validSites = ["youtube.com", "facebook.com"]
+    let notValidSites = ["virus.com", "stepn-eventos.com", "spin2money.cc"]
+    //  for(let i = 0;i<100000;i++){      console.log(i)  }
     console.log("logged  " + data)
-   validSites.forEach(element=>{
-    if(data.includes(element)){
-      console.log(element)
-      finished=true;
-      notFound=false
-      this.postMessage("Valid")
+    validSites.forEach(element => {
+        if (data.includes(element)) {
+            console.log(element)
+            finished = true;
+            notFound = false
+            this.postMessage("Valid")
+        }
+    })
+    if (!finished) {
+        notValidSites.forEach(element => {
+            if (data.includes(element)) {
+                // console.log(element)
+                finished = true
+                notFound = false
+                this.postMessage("NotValid")
+            }
+        })
     }
-   })
-   if(!finished){
-     notValidSites.forEach(element=>{
-       if(data.includes(element)){
-       // console.log(element)
-         finished = true
-         notFound=false
-         this.postMessage("NotValid")
-       }
-     })
-   }
-   console.log(notFound)
-   if(notFound==true){
-     this.postMessage("Not Found")
-   }
+    console.log(notFound)
+    if (notFound == true) {
+        this.postMessage("Not Found")
+    }
 }
